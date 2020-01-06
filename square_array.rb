dbc810bf137024ccdc86c7cd226879e099759b06
Jared Fleming <jaredmf10@gmail.com>
2020-01-06 15:36:37 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,6 @@ def square_array(array)
   # your code here
   # call on each number
   # numbers = [1, 2, 3]
-  # numbers.each{|x| puts x}
   # square each of the numbers
   num_square = []
   array.each {|num|
